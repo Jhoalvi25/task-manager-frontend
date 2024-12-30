62ffa529be7913d812da1f790e0b6d3abe15c873
Jhoalvi25 <jhoalvipereira@gmail.com>
2024-12-30 06:22:31 -0500

    loading with message

--- src/components/LoadingSpinner.jsx
+++ src/components/LoadingSpinner.jsx
@@ -2,15 +2,18 @@ import React from "react";
 
 const LoadingSpinner = () => {
   return (
-    <div className="flex items-center justify-center h-[50vh] bg-gray-100">
+    <div className="flex flex-col items-center justify-center h-[50vh] bg-gray-100">
+      {/* Spinner Animado */}
       <div className="flex items-center justify-center space-x-2">
         <div className="w-4 h-4 rounded-full bg-blue-500 animate-bounce"></div>
         <div className="w-4 h-4 rounded-full bg-blue-500 animate-bounce delay-200"></div>
         <div className="w-4 h-4 rounded-full bg-blue-500 animate-bounce delay-400"></div>
       </div>
+      <p className="mt-4 text-gray-700 text-center text-sm md:text-base">
+        Loading, please be patient...
+      </p>
     </div>
   );
 };
 
 export default LoadingSpinner;
-
